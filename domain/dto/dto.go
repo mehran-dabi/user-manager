2ba8506a768a10b0f67f106b91751b3a0edf5a51
Mehran Dabestani <mehran.dabes@gmail.com>
2022-10-17 19:07:26 +0330

    Add user and filter dto

---
+++ domain/dto/dto.go
@@ -0,0 +1,22 @@
+package dto
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	NickName  string    `json:"nick_name"`
+	Email     string    `json:"email"`
+	Country   string    `json:"country"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Filter struct {
+	Country   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
