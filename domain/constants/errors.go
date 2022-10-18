22cff30f2f8860242db0af8ebcf3161e0e97e032
Mehran Dabestani <mehran.dabes@gmail.com>
2022-10-18 11:09:49 +0330

    Add user exists error

--- domain/constants/errors.go
+++ domain/constants/errors.go
@@ -5,4 +5,5 @@ import "fmt"
 var (
 	ErrUserNotFound = fmt.Errorf("user not found")
 	ErrHasNoChanges = fmt.Errorf("the information has no changes")
+	ErrUserExists   = fmt.Errorf("user already exists")
 )
